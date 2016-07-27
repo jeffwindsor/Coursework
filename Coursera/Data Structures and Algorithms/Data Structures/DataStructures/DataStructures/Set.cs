3a21645587f5832d1c5926be6f8973afd85543c7
Jeff Windsor <jeff.windsor@gmail.com>
2016-07-27 16:52:24 -0600

    interim

---
+++ Coursera/Data Structures and Algorithms/Data Structures/DataStructures/DataStructures/Set.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Uses Hash and Chaining
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class Set<TValue>
+        where TValue : class, IEquatable<TValue>
+    {
+        private readonly List<TValue>[] _lists;
+        private readonly Func<TValue, int> _hashFunction;
+        public Set(int size, Func<TValue, int> hashFunction)
+        {
+            _lists = new List<TValue>[size];
+            _hashFunction = hashFunction;
+        }
+
+        public bool Find(TValue value)
+        {
+            return (FindValue(value) != null);
+        }
+
+        public void Add(TValue value)
+        {
+            var l = GetList(value);
+            if(FindValue(value,l) == null)
+                l.Add(value);
+        }
+
+        public void Remove(TValue value)
+        {
+            var l = GetList(value);
+            if (FindValue(value, l) == null)
+                l.Remove(value);
+        }
+
+
+        private List<TValue> GetList(TValue value)
+        {
+            return _lists[_hashFunction(value)];
+        }
+
+        private TValue FindValue(TValue value)
+        {
+            return FindValue(value, GetList(value));
+        }
+        private static TValue FindValue(TValue value, IEnumerable<TValue> list)
+        {
+            return list.FirstOrDefault(o => o.Equals(value));
+        }
+    }
+}
--- Coursera/Data Structures and Algorithms/Data Structures/DataStructures/DataStructures/W3/Phonebook.cs
+++ Coursera/Data Structures and Algorithms/Data Structures/DataStructures/DataStructures/W3/Phonebook.cs
@@ -10,6 +10,7 @@ namespace DataStructures.W3
         {
             Process(Phonebook.Process);
         }
+
         private static void Process(Func<string[], string[]> process)
         {
             var input = new List<string>();
@@ -28,33 +29,98 @@ namespace DataStructures.W3
 
     public class Phonebook
     {
+        const string NOT_FOUND = "not found";
+        const string ADD = "add";
+        const string FIND = "find";
+        const string DEL = "del";
+
         public static string[] Process(string[] inputs)
         {
-            var chars = new[] { ' ' };
+            var chars = new[] {' '};
             var queryCount = int.Parse(inputs[0]);
             var queries = inputs.Skip(1).Take(queryCount).Select(l =>
             {
                 var splits = l.Split(chars);
-                return new Query(splits[0], splits[1], (splits.Length == 3) ? splits[2]: string.Empty);
+                return new Query(splits[0], splits[1], (splits.Length == 3) ? splits[2] : string.Empty);
             });
 
             return Process(queries)
                 .ToArray();
         }
+
         public static IEnumerable<string> Process(IEnumerable<Query> queries)
         {
-            throw new NotImplementedException();
+            var pb = new Phonebook();
+            var resutls = new List<string>();
+            foreach (var query in queries)
+            {
+                switch (query.Command)
+                {
+                    case ADD:
+                        pb.Add(query.Number, query.Name);
+                        break;
+                    case FIND:
+                        pb.FindName(query.Number);
+                        break;
+                    case DEL:
+                        break;
+                    default:
+                        throw new ArgumentException("Command Not Known");
+                }
+            }
         }
+
+
+        
+        private readonly Dictionary<string, int> _names = new Dictionary<string, int>();
+        private readonly Dictionary<int, string> _numbers = new Dictionary<int, string>();
+
+        public void Add(int number, string name)
+        {
+            _names[name] = number;
+            _numbers[number] = name;
+        }
+
+        public void Remove(int number)
+        {
+            if (!_numbers.ContainsKey(number)) return;
+
+            var name = _numbers[number];
+            _numbers.Remove(number);
+            _names.Remove(name);
+        }
+
+        public string FindNumber(string name)
+        {
+            return (_names.ContainsKey(name))
+                ? _names[name].ToString()
+                : NOT_FOUND;
+        }
+
+        public string FindName(int number)
+        {
+            return (_numbers.ContainsKey(number))
+                ? _numbers[number]
+                : NOT_FOUND;
+        }
+
 
         public class Query
         {
-            public Query(string c, string num, string name) { Command = c; Number = num; Name = name; }
+            public Query(string c, string num, string name)
+            {
+                Command = c.ToLower();
+                Number = num;
+                Name = name;
+            }
+
             public string Command;
             public string Number;
             public string Name;
+
             public override string ToString()
             {
-                return string.Format("{0} {1} {2}", Command, Number, Name);
+                return $"{Command} {Number} {Name}";
             }
         }
     }
