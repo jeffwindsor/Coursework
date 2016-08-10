a84476fe661291e15235e145fabcf27a331cfaf5
Jeff Windsor <jeff.windsor@gmail.com>
2016-08-10 13:47:46 -0600

    DS W5 tree-orders complete

--- Coursera/Data Structures and Algorithms/Data Structures/W4 - Binary Search Trees/1tree_orders/tree-orders.cs
+++ Coursera/Data Structures and Algorithms/Data Structures/W4 - Binary Search Trees/1tree_orders/tree-orders.cs
@@ -6,16 +6,16 @@ namespace DataStructures.W4and5
 {
     public class TreeOrder
     {
-        //public static void Main(string[] args)
-        //{
-        //    string s;
-        //    var inputs = new List<string>();
-        //    while ((s = Console.ReadLine()) != null)
-        //        inputs.Add(s);
+        public static void Main(string[] args)
+        {
+            string s;
+            var inputs = new List<string>();
+            while ((s = Console.ReadLine()) != null)
+                inputs.Add(s);
 
-        //    foreach (var result in Answer(inputs.ToArray()))
-        //        Console.WriteLine(result);
-        //}
+            foreach (var result in Answer(inputs.ToArray()))
+                Console.WriteLine(result);
+        }
 
         //Use this for test input
         public static IList<string> Answer(IList<string> inputs)
@@ -41,7 +41,7 @@ namespace DataStructures.W4and5
             return results;
         }
 
-        private const int NullId = -1;
+        const int NullId = -1;
         private readonly Node[] _nodes;
         public TreeOrder(IEnumerable<Node> nodes){ _nodes = nodes.ToArray();}
 
