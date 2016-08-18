1027a6dff8441b6a576014175982dc977d64541e
Jeff Windsor <jeff.windsor@gmail.com>
2016-08-18 15:06:22 -0600

    DSW5 Rope Green no rebalance

--- Coursera/Data Structures and Algorithms/Data Structures/W4 - Binary Search Trees/3rope/rope.cs
+++ Coursera/Data Structures and Algorithms/Data Structures/W4 - Binary Search Trees/3rope/rope.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -163,16 +163,16 @@ namespace DataStructures.W4and5
         }
         
         #region Main
-        //public static void Main(string[] args)
-        //{
-        //    string s;
-        //    var inputs = new List<string>();
-        //    while ((s = Console.ReadLine()) != null)
-        //        inputs.Add(s);
+        public static void Main(string[] args)
+        {
+            string s;
+            var inputs = new List<string>();
+            while ((s = Console.ReadLine()) != null)
+                inputs.Add(s);
 
-        //    foreach (var result in Answer(inputs.ToArray()))
-        //        Console.WriteLine(result);
-        //}
+            foreach (var result in Answer(inputs.ToArray()))
+                Console.WriteLine(result);
+        }
 
         public static IList<string> Answer(IList<string> inputs)
         {
