6f063b9c657b80756e592eefea5e28c155b04b9d
jwindsor <jwindsor@cj.com>
2018-08-12 09:07:19 -0700

    Ch5 further

--- Books/HaskellPFFP/Ch5.hs
+++ Books/HaskellPFFP/Ch5.hs
@@ -1,14 +1,45 @@
-
 {- 
+Chapter Exercises
 
-Exercise: Type Matching
-head :: [a] -> a
-concat :: [[a]] -> [a] 
-not :: Bool -> Bool
-length :: [a] -> Int
-(<) :: Ord a => a -> a -> Bool
+Multiple Choice
+1:c 2:a 3:b 4:c
 
-Exercise: Type Arguments
-1:a 2:d 3:d 4:c 5:a 6:e 7:e 8:e 9:c 
+Determine the type
+1:
+    a: Num a=>a 54
+    b: (Num a, [Char])
+    c: (Int, [Char])
+    d: Bool
+    e: Int
+    f: Bool
+
+2: Num a => a
+3: Num a => a -> a
+4: Fractional a => a
+5: [Char]
+
+Does it compile
+1:No
+2:yes
+3:No
+4:No
+
+Type variable or specific type constructor?
+1: constrained, fully, fully, con, con
+2: con, con
+3: constained, full, full, con
+4: full, full, con
+
+Write a type signature
+
+functionH :: [a] => a
+functionH (x:_) = x
+
+functionC :: Ord a => a -> a -> Bool
+functionC x y = if (x > y) then True else False
+
+functionS :: (a,b) => b
+functionS (x, y) = y
+
 
 -}
