895a46a956e0930bda9e798abff052a953db8cd5
Jeff Windsor <jeffwindsor@tardis.lan>
2018-08-13 20:30:55 -0700

    Chapter5 consolidate

--- Books/HaskellPFFP/CH5.hs
+++ Books/HaskellPFFP/CH5.hs
@@ -31,6 +31,7 @@ Type variable or specific type constructor?
 4: full, full, con
 -} 
 
+-------------------------------------------------
 --Write a type signature
 functionH :: [a] -> a
 functionH (x:_) = x
@@ -41,6 +42,7 @@ functionC x y = if (x > y) then True else False
 functionS :: (a,b) -> b
 functionS (x, y) = y
 
+-------------------------------------------------
 --Given a type, write the function
 --1. Thereisonlyonefunctiondefinitionthattypechecksanddoesn’t go into an infinite loop when you run it.
 i :: a -> a
@@ -73,3 +75,78 @@ a _ a = a  --why is this not a2b
 --8. One version will typecheck.
 a' :: (a -> b) -> a -> b 
 a' a2b a = a2b a 
+
+-------------------------------------------------
+--Fixit
+
+--Sing
+fstString :: [Char] -> [Char]
+fstString x = x ++ " in the rain"
+
+sndString :: [Char] -> [Char]
+sndString x = x ++ " over the rainbow"
+
+rainbow = if (x > y) then fstString x else sndString y
+    where x = "Singin"
+          y = "Somewhere"
+
+rain = if (x < y) then fstString x else sndString y
+    where x = "Singin"
+          y = "Somewhere"
+
+--Artih3Broken
+main :: IO () 
+main = do
+    print $ 1 + 2
+    print 10
+    print $ negate (-1)
+    print $ (+) 0 blah
+        where blah = negate 1
+
+
+-------------------------------------------------
+--TypeKwonDo
+--1
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h a = g $ f a
+
+--2
+data A 
+data B 
+data C
+
+q :: A -> B
+q = undefined
+
+w :: B -> C
+w = undefined
+
+e :: A -> C
+e a = w $ q a
+
+--3. 
+data X 
+data Y 
+data Z
+
+xz :: X -> Z 
+xz = undefined
+
+yz :: Y -> Z 
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z) 
+xform (x,y) = (xz x, yz y)
+
+--4. 
+munge :: (x -> y)
+        -> (y -> (w, z))
+        -> x
+        -> w 
+munge x2y y2wz x = fst $ y2wz $ x2y x
