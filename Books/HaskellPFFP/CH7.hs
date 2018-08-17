ace78b20514ec833192ac362fd9899c9a7abc46b
jwindsor <jwindsor@cj.com>
2018-08-17 16:17:16 -0700

    Haskell Ch7 updates and 9 start

--- Books/HaskellPFFP/CH7.hs
+++ Books/HaskellPFFP/CH7.hs
@@ -1,45 +1,29 @@
---LET IN SYNTAX
+--LET.IN SYNTAX
 bindExp :: Integer -> String 
 bindExp x =
-    let z = y + x; y = 5 in
-            "the integer was: " ++ 
-            show x ++ 
-            " and y was: " ++ 
-            show y ++ 
-            " and z was: " ++ 
-            show z
-    
---LAMBDA SYNTAX 
-ls = (\x -> x * 3) :: Integer -> Integer
+    let z = y + x
+        y = 5 
+    in "answer: " ++ show x ++ " : " ++ show z
 
---Exercises: Grab Bag
-{-
-1. Which (two or more) of the following are equivalent?  --ALL OF THEM
-*    a) mTh x y z = x * y * z
-*    b) mTh x y = \z -> x * y * z
-*    c) mTh x = \y -> \z -> x * y * z
-*    d) mTh = \x -> \y -> \z -> x * y * z
+--PATTERN MATCH SYNTAX
+triple :: Integer -> Integer 
+triple 0 = 0  --shown for contrast to catch all case
+triple x = x * 3
 
-2. ThetypeofmTh(above)isNum a => a -> a -> a -> a. Which is the type of mTh 3?
-*   a) Integer -> Integer -> Integer 
-    b) Num a => a -> a -> a -> a
-    c) Num a => a -> a
-    d) Num a => a -> a -> a
- 
-3. Next, we’ll practice writing anonymous lambda syntax. For example, one could rewrite:
-        addOne x = x + 1
-    Into:
-        addOne = \x -> x + 1
-    Try to make it so it can still be loaded as a top-level definition by GHCi. This will make it easier to validate your answers.
-    a) Rewrite the f function in the where clause.
-    
+--LAMBDA SYNTAX
+anonymousTriple = (\x -> x * 3) :: Integer -> Integer
+
+--CASE SYNTAX
+
+
+{-  
+Exercises: Grab Bag PG224
+    1. a,b,c,d
+    2. a
+    3.a.
         addOneIfOdd n = case odd n of 
             True  -> n + 1
             False -> n
-
-    b) Rewrite the following to use anonymous lambda syntax:
-        addFive = \x -> \y -> (if x > y then y else x) + 5
-
-    c) Rewrite the following so that it doesn’t use anonymous lambda syntax:
-        mflip f x y = f y x
+    3.b addFive = \x -> \y -> (if x > y then y else x) + 5
+    3.c mflip f x y = f y x
 -}
---
+++ Books/HaskellPFFP/CH9.hs
@@ -0,0 +1,22 @@
+
+
+--PG 307 >> Exercise: EnumFromTo - provide an enumFromTo implementation
+eftBool :: Bool -> Bool -> [Bool]
+eftBool True False = [True, False]
+eftBool False True = [False, True]
+eftBool n _ = [n]
+
+eftOrd :: Ordering
+       -> Ordering
+       -> [Ordering] 
+eftOrd = undefined
+-- ???
+
+eftInt :: Int -> Int -> [Int] 
+eftInt n m
+    | n > m  = []
+    | n <= m = n : eftInt (n+1) m
+
+eftChar :: Char -> Char -> [Char] 
+eftChar = undefined
+-- ???
