385c723788d23c1f1a687781bb0c5dd4c532ad30
jwindsor <jwindsor@cj.com>
2018-08-18 19:05:44 -0700

    Lines

--- machine-setup.sh
+++ machine-setup.sh
@@ -40,6 +40,18 @@ warning(){ printcln_f 226 "${1}"; }
 error(){ printcln_f 196 "${1}"; }
 detail(){ printcln_f 244 "${1}"; }
 
+# for fgbg in 38 48 ; do # Foreground / Background
+#     for color in {0..255} ; do # Colors
+#         # Display the color
+#         printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+#         # Display 6 colors per lines
+#         if [ $((($color + 1) % 6)) == 4 ] ; then
+#             echo # New line
+#         fi
+#     done
+#     echo # New line
+# done
+
 ###############################################################################
 ## OS IDENTIFICATION - NOT SURE THIS IS THE BEST YET
 uname=$(uname);
